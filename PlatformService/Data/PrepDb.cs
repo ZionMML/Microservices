2675f5e5f1b18eb6c2131fbcc452e1a2cd316882
zion66682 <zion82@gmail.com>
2025-08-25 21:27:41 +1200

    Initial commit - added CommandService, K8S, PlatformService

---
+++ PlatformService/Data/PrepDb.cs
@@ -0,0 +1,35 @@
+using Microsoft.IdentityModel.Tokens;
+using PlatformService.Models;
+
+namespace PlatformService.Data;
+
+public static class PrepDb
+{
+    public static void PrepPopulation(IApplicationBuilder app)
+    {
+        using (var serviceScope = app.ApplicationServices.CreateScope())
+        {
+            SeedData(serviceScope.ServiceProvider.GetRequiredService<AppDbContext>());
+        }
+    }
+
+    private static void SeedData(AppDbContext context)
+    {
+        if (!context.Platforms.Any())
+        {
+            Console.WriteLine("--> Sedding Data...");
+
+            context.Platforms.AddRange(
+                new Platform() { Id = 1, Name = "Dot Net", Publisher = "Microsoft", Cost = "Free" },
+                new Platform() { Id = 2, Name = "SQL Server Express", Publisher = "Microsoft", Cost = "Free" },
+                new Platform() { Id = 3, Name = "Kubernetes", Publisher = "Cloud Native Coomputing Foundation", Cost = "Free" }
+            );
+
+            context.SaveChanges();
+        }
+        else
+        {
+            Console.WriteLine("--> We already have data");
+        }
+    }
+}
---
+++ PlatformService/Models/Platform.cs
@@ -0,0 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace PlatformService.Models
+{
+    public class Platform
+    {
+        [Key]
+        public required int Id { get; set; }
+        public required string Name { get; set; }
+        public required string Publisher { get; set; }
+
+        public required string Cost { get; set; }
+    }
+}
